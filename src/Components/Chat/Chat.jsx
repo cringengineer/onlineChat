5582c4ef8ae6e1b189ad3be67b5fe62248888299
cringengineer <cringengineer@gmail.com>
2023-01-22 15:40:06 +0300

    Merge remote-tracking branch 'origin/master'

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -63,7 +63,7 @@ const StyledForm = styled.form`
   width: 60vw;
   align-items: center;
   justify-content: space-between;
-  height: 40px;
+  height: 60px;
   border-radius: 0 0 15px 15px;
   @media (max-width: 768px) {
     width: 100vw;
